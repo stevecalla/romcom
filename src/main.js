6eb9751f589bd63a834de316e72508416ad8deb9
Steve Calla <callasteven@gmail.com>
2020-12-12 12:37:20 -0700

    Fix make cover code and eliminate save cover duplicates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 //Create variables targetting the relevant DOM elements here 👇
 
 var coverImage1 = document.querySelector("img");  //row19
+var coverTitle = document.querySelector(".cover-title"); // row 20
 var tagline1 = document.querySelector(".tagline-1"); //row21
 var tagline2 = document.querySelector(".tagline-2"); // row21
-var coverTitle = document.querySelector(".cover-title"); // row 20
 var randomButtonCover = document.querySelector('.random-cover-button'); //row 12
 
 function getRandomIndex(array) {
@@ -17,11 +17,13 @@ function randomCover() {
   coverTitle.innerText = titles[getRandomIndex(titles)];
   tagline1.innerText = descriptors[getRandomIndex(descriptors)];
   tagline2.innerText = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(coverImage1, coverTitle, tagline1, tagline2);
 }
 
 var homeView = document.querySelector(".view.home-view");
 var makeCoverView = document.querySelector(".view.form-view.hidden");
 var saveCoverView = document.querySelector(".view.saved-view.hidden");
+var savedCoversSection = document.querySelector(".saved-covers-section");
 
 var makeCoverButton = document.querySelector(".make-new-button");
 var randomCoverButton = document.querySelector(".random-cover-button");
@@ -29,8 +31,6 @@ var saveCoverButton = document.querySelector(".save-cover-button")
 var savedViewCoverButton = document.querySelector(".view-saved-button")
 var homeCoverButton = document.querySelector(".home-button.hidden")
 var createNewSavedBook = document.querySelector(".create-new-book-button");
-
-
 
 makeCoverButton.addEventListener("click", viewSwitchMakeCover);
 
@@ -53,6 +53,13 @@ function viewSwitchSaveCover() {
   saveCoverButton.classList.add('hidden');
   homeCoverButton.classList.remove('hidden');
   makeCoverView.classList.add('hidden');
+  // display saved covers array
+  // savedCoversSection.innerHTML =
+  //   `<img class="cover-image" src="./assets/prairie.jpg">
+  //   <h2 class="cover-title">Windswept Hearts</h2>
+  //   <h3 class="tagline">A tale of <span class="tagline-1">passion</span> and <span class="tagline-2">woe</span></h3>
+  //   <img class="price-tag" src="./assets/price.png">
+  //   <img class="overlay" src="./assets/overlay.png">`;
 };
 
 homeCoverButton.addEventListener("click", viewSwitchHomeCover);
@@ -73,6 +80,7 @@ var savedCovers = [
 var currentCover;
 
 var coverInput = document.getElementById("cover");
+console.log('what is this =', coverInput);
 var titleInput = document.getElementById("title");
 var descriptor1Input = document.getElementById("descriptor1");
 var descriptor2Input = document.getElementById("descriptor2");
@@ -81,39 +89,36 @@ createNewSavedBook.addEventListener("click", createNewBook);
 
 function createNewBook() {
   event.preventDefault()
-  var coverInputImage = coverInput.value;
-  var titleInputText = titleInput.value;
-  var descriptor1Text = descriptor1Input.value;
-  var descriptor2Text = descriptor2Input.value;
+  coverInputImage = coverInput.value;
+  titleInputText = titleInput.value;
+  descriptor1Text = descriptor1Input.value;
+  descriptor2Text = descriptor2Input.value;
+  currentCover = new Cover(coverInputImage, titleInputText, descriptor1Text, descriptor2Text);
+  coverImage1.src = coverInputImage;
   covers.push(coverInputImage);
   titles.push(titleInputText);
   descriptors.push(descriptor1Text, descriptor2Input);
-  var currentCover = new Cover(coverInputImage, titleInputText, descriptor1Text, descriptor2Text);
-  viewSwitchHomeCover()
-  coverImage1.src = `${coverInputImage}`;
   coverTitle.innerText = titleInputText;
   tagline1.innerText = descriptor1Text;
   tagline2.innerText = descriptor2Text;
+  viewSwitchHomeCover()
+
+//these are probably out order
+//create a new instance (input values, create new instance,
+//store new instance, switch to homepage, display, then if user likes
+//it they'll save it, then push into poster)
 };
 
 saveCoverButton.addEventListener("click", saveDisplayCurrentCover);
 
-function saveDisplayCurrentCover(currentCover) {
-  currentCover = new Cover(coverImage1, coverTitle, tagline1, tagline2);
-  console.log('saved', savedCovers);
-  console.log('current', currentCover);
-  savedCovers.push(currentCover);
-  console.log(savedCovers.indexOf(currentCover));
-  console.log(coverImage1,coverTitle, tagline1, tagline2);
-  if (savedCovers.includes(currentCover)) {
-    return "hello";
-  } else {
-    savedCovers.push(currentCover);
-  };
-  // console.log(savedCovers.indexOf(currentCover));
-  console.log(savedCovers);
+function saveDisplayCurrentCover() {
+  console.log('current cover', currentCover);
+  console.log('saved cover =', savedCovers);
+  if (!savedCovers.includes(currentCover)) {  //! is the same as "=== false"
+      savedCovers.push(currentCover);
+  }
+  console.log('after push = ', savedCovers);
 };
-
 
 // Add your event listeners here 👇
 
